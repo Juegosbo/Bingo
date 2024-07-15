1a7d86efc90b7b95889710c587ed8acdd09d25e2
Juegosbo <juegosbo24@gmail.com>
2024-07-15 16:40:07 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -479,7 +479,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     false, false, true,  false, false,
                     false, true,  true,  true,  false,
                     true,  true, true,  true, true,
-                    false, false,  true,  false,  false,
+                    false, true,  true,  false,  false,
                     false, false, true,  false, false
                 ];
                 break;
