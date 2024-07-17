4c9a95363de3b8d9d48cdf837d8a39bd6099ebdb
Juegosbo <juegosbo24@gmail.com>
2024-07-17 15:32:44 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -509,29 +509,29 @@ document.addEventListener('DOMContentLoaded', () => {
             break;
         case 'letterH':
             cells = [
-                true, false, false, false, true,
-                true, false, false, false, true,
                 true, true, true, true, true,
-                true, false, false, false, true,
-                true, false, false, false, true
+                false, false, true, false, false,
+                false, false, true, false, false,
+                false, false, true, false, false,
+                true, true, true, true, true
             ];
             break;
         case 'tree':
             cells = [
                 false, false, true,  false, false,
-                false, true,  true,  true,  false,
+                false, true,  true,  false,  false,
                 true,  true, true,  true, true,
-                false, false,  true,  false,  false,
+                false, true,  true,  false,  false,
                 false, false, true,  false, false
             ];
             break;
         case 'numberOne':
             cells = [
-                false, false, true,  false, false,
-                false, true, true,  false, false,
-                false, false, true,  false, false,
-                false, false, true,  false, false,
-                false,  true,  true,  true,  false
+               false, false, false,  false, false,
+               false, true, false,  false, true,
+               true, true, true,  true, true,
+               false, false, false,  false, true,
+               false,  false,  false,  false,  false
             ];
             break;
         case 'chess':
