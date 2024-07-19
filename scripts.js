ab35ed4e4308475a06ec540589828a818e4b1b13
Juegosbo <juegosbo24@gmail.com>
2024-07-19 14:52:03 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -491,11 +491,11 @@ function updateFigurePreview(figure) {
             break;
         case 'letterH':
             cells = [
-                true, false, true, false, true,
-                true, false, true, false, true,
+                false, false, false, false, false,
+                false, false, true, false, false,
+                true, true, true, true, false,
                 true, true, true, true, true,
-                true, false, true, false, true,
-                true, false, true, false, true
+                false, false, false, false, false
             ];
             break;
         case 'tree':
