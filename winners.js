6317d1429b3fc52166c3dc7ebee60fe55dd48fcc
Juegosbo <juegosbo24@gmail.com>
2024-07-29 12:16:48 -0400

    Update winners.js

--- winners.js
+++ winners.js
@@ -92,6 +92,13 @@ document.addEventListener('DOMContentLoaded', () => {
             false, true, true, true, false,
             false, false, false, false, false
         ],
+        'U': [
+            true, true, true,  true, true,
+            false, false, false,  false, true,
+            false,  false,  false,  false,  true,
+            false, false, false,  false, true,
+            true, true, true,  true, true
+        ],
 
         'O': [
             true, true, true, true, true,
