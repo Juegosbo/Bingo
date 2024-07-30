f256bfa83e7f6471aafb2406805b40a7b517a557
Juegosbo <juegosbo24@gmail.com>
2024-07-30 12:24:11 -0400

    Update winners.js

--- winners.js
+++ winners.js
@@ -102,11 +102,11 @@ document.addEventListener('DOMContentLoaded', () => {
             false, false, false, false, false
         ],
         'letU': [
-            true, true, true,  true, true,
-            false, false, false,  false, true,
-            false,  false,  false,  false,  true,
-            false, false, false,  false, true,
-            true, true, true,  true, true
+            true, true, true, true, true,
+            false, false, false, false, true,
+            false, false, false, false, true,
+            false, false, false, false, true,
+            true, true, true, true, true
         ],
          'J': [
             false, false, false,  false, false,
@@ -131,9 +131,9 @@ document.addEventListener('DOMContentLoaded', () => {
         ],
         'Numerodos': [
            false,  false, false, false, false,
-            true, true,  false, true,  true,
-            true, false, false,  false, true,
             true, true,  false, false,  true,
+            true, false, false,  false, true,
+            true, false,  false, true,  true,
             false,  false, false, false, false
         ],
         'Numerotres': [
@@ -158,11 +158,11 @@ document.addEventListener('DOMContentLoaded', () => {
             true,  true, true, true, true
         ],
         'letE': [
-           true,  false, true, false, true,
+            true,  true, true, true, true,
             true, false,  true, false,  true,
             true, false, false,  false, true,
             true, false,  true, false,  true,
-            true,  true, true, true, true
+            true,  false, true, false, true
         ],
 
         'O': [
